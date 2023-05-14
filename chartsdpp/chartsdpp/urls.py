93cab2a2dbbc2c33e94c50c5d95c363690ead931
Vini347 <contato.viniciusdasilva@gmail.com>
2023-05-14 18:40:08 -0300

    att

---
+++ chartsdpp/chartsdpp/urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for chartsdpp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import Chart
+from .views import Dash_IncidenteArea,Dash_IncidenteAreaPizza
+from .views import Dash_PoliticasManuais, Dash_PoliticasManuaisTabela
+from .views import Dash_CartaoNorma,Dash_TabelaNorma
+from .views import Dash_DemandasCartao,Dash_DemandasTabela
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/',Chart),
+    path('IncidenteArea/',Dash_IncidenteArea),
+    path('IncidenteAreaPizza/',Dash_IncidenteAreaPizza),
+    path('PoliticasManuais/',Dash_PoliticasManuais),
+    path('PoliticasManuaisTabela/',Dash_PoliticasManuaisTabela),
+    path('CartaoNorma/',Dash_CartaoNorma),
+    path('TabelaNorma/',Dash_TabelaNorma),
+    path('DemandasCartao/',Dash_DemandasCartao),
+    path('DemandasTabela/',Dash_DemandasTabela)
+
+]
